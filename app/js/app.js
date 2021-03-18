d045b361ec1c43452c414a992681cc2f49857ec3
n1colashka <deckards2505@gmail.com>
2021-03-18 21:56:01 +0300

    fix: fix

--- app/js/app.js
+++ app/js/app.js
@@ -383,34 +383,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 // touchRatio: 0
             });
         }
-        var checkScroll = function(evt){
-            var $slide = $(this),
-                scrollTop = $slide.scrollTop(); 
-            if (scrollTop > 0 && (scrollTop + $slide.height()) < $slide[0].scrollHeight) {
-                evt.stopPropagation();
-            }
-        };
-        
-        var startScroll, touchStart, touchCurrent;
-        swiper.slides.on('touchstart', function (e) {
-            startScroll = this.scrollTop;
-            touchStart = e.targetTouches[0].pageY;
-        }, true);
-        swiper.slides.on('touchmove', function (e) {
-            touchCurrent = e.targetTouches[0].pageY;
-            var touchesDiff = touchCurrent - touchStart;
-            var slide = this;
-            var onlyScrolling = 
-                    ( slide.scrollHeight > slide.offsetHeight ) && //allow only when slide is scrollable
-                    (
-                        ( touchesDiff < 0 && startScroll === 0 ) || //start from top edge to scroll bottom
-                        ( touchesDiff > 0 && startScroll === ( slide.scrollHeight - slide.offsetHeight ) ) || //start from bottom edge to scroll top
-                        ( startScroll > 0 && startScroll < ( slide.scrollHeight - slide.offsetHeight ) ) //start from the middle
-                    );
-            if (onlyScrolling) {
-                e.stopPropagation();
-            }
-        }, true);
     }
 
     function initSelect() {
